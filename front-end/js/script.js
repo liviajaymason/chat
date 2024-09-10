46621fd7d43df4a81a3d9756d0d94da4d3a3f543
liviajaymason <liviajaymason@gmail.com>
2024-09-10 19:51:25 +0900

    Update script.js

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -119,7 +119,7 @@ const handleLogin = async (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-dzys.onrender.com/")
 
     websocket.onmessage = processMessage
 }
